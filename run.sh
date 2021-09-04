e67e2358efd5fa2c5d8f5efc0cad4df7a9aa3278
tudo75 <tudo75@hotmail.com>
2021-09-04 02:44:25 +0200

    first commit without vala sources

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+meson setup build --prefix=/usr --wipe
+# ninja -C build -v com.github.tudo75.xed-sessionsaver-plugin-pot
+# ninja -C build -v com.github.tudo75.xed-sessionsaver-plugin-update-po
+# ninja -C build -v com.github.tudo75.xed-sessionsaver-plugin-gmo
+ninja -v -C build
+ninja -v -C build install
